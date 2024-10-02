e163401de83995e6ddad68cf8715077a672824fa
myusername <himashawije5678@gmail.com>
2024-10-02 11:37:16 +0530

    validated user input fields

--- resources/views/batches/show.blade.php
+++ resources/views/batches/show.blade.php
@@ -9,7 +9,7 @@
  
         <div class="card-body">
         <h5 class="card-title">Name : {{ $batch->name }}</h5>
-        <p class="card-text">Course : {{ $batch->course_id }}</p>
+        <p class="card-text">Course : {{ $batch->course->name }}</p>
         <p class="card-text">Start Date : {{ $batch->start_date }}</p>
   </div>
        
--- resources/views/enrollments/index.blade.php
+++ resources/views/enrollments/index.blade.php
@@ -29,8 +29,8 @@
                                     <tr>
                                         <td>{{ $loop->iteration }}</td>
                                         <td>{{ $item->name }}</td>
-                                        <td>{{ $item->batch_id }}</td>
-                                        <td>{{ $item->student_id }}</td>
+                                        <td>{{ $item->batch->name }}</td>
+                                        <td>{{ $item->student->name }}</td>
                                         <td>{{ $item->join_date }}</td>
                                         <td>{{ $item->fee }}</td>
 
